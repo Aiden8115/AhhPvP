459bc4ba8ed65850193686362b2cdc6db6e9c20c
Aiden8115 <3967083449@qq.com>
2025-12-19 16:54:53 +0800

    change (block egg -> bridge egg)

---
+++ src/main/java/com/aiden/pvp/blocks/EggBridgeBlock.java
@@ -0,0 +1,9 @@
+package com.aiden.pvp.blocks;
+
+import net.minecraft.block.Block;
+
+public class EggBridgeBlock extends Block {
+    public EggBridgeBlock(Settings settings) {
+        super(settings);
+    }
+}
