247272c5fb2838f26a9488a7ef122c0a4ffffe28
Aiden8115 <3967083449@qq.com>
2026-01-06 21:28:01 +0800

    change the murderer's ai

--- src/main/java/com/aiden/pvp/entities/MurdererEntity.java
+++ src/main/java/com/aiden/pvp/entities/MurdererEntity.java
@@ -3,6 +3,7 @@ package com.aiden.pvp.entities;
 import com.aiden.pvp.items.ModItems;
 import net.minecraft.entity.*;
 import net.minecraft.entity.ai.goal.*;
+import net.minecraft.entity.ai.pathing.Path;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.mob.*;
@@ -15,8 +16,10 @@ import net.minecraft.entity.projectile.ProjectileUtil;
 import net.minecraft.item.BowItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.predicate.entity.EntityPredicates;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.LocalDifficulty;
@@ -109,6 +112,149 @@ public class MurdererEntity extends HostileEntity {
         CROSSBOW_CHARGE,
         CELEBRATING,
         NEUTRAL;
+    }
+
+    static class MeleeAttackGoal extends Goal {
+        protected final PathAwareEntity mob;
+        private final double speed;
+        private final boolean pauseWhenMobIdle;
+        private Path path;
+        private double targetX;
+        private double targetY;
+        private double targetZ;
+        private int updateCountdownTicks;
+        private int cooldown;
+        private final int attackIntervalTicks = 20;
+        private long lastUpdateTime;
+        private static final long MAX_ATTACK_TIME = 20L;
+
+        public MeleeAttackGoal(PathAwareEntity mob, double speed, boolean pauseWhenMobIdle) {
+            this.mob = mob;
+            this.speed = speed;
+            this.pauseWhenMobIdle = pauseWhenMobIdle;
+            this.setControls(EnumSet.of(Goal.Control.MOVE, Goal.Control.LOOK));
+        }
+
+        @Override
+        public boolean canStart() {
+            long l = this.mob.getEntityWorld().getTime();
+            if (l - this.lastUpdateTime < 20L) {
+                return false;
+            } else {
+                this.lastUpdateTime = l;
+                LivingEntity livingEntity = this.mob.getTarget();
+                if (livingEntity == null) {
+                    return false;
+                } else if (!livingEntity.isAlive()) {
+                    return false;
+                } else {
+                    this.path = this.mob.getNavigation().findPathTo(livingEntity, 0);
+                    return (this.path != null || this.mob.isInAttackRange(livingEntity)) && this.mob.squaredDistanceTo(livingEntity) <= 100;
+                }
+            }
+        }
+
+        @Override
+        public boolean shouldContinue() {
+            LivingEntity livingEntity = this.mob.getTarget();
+            if (livingEntity == null) {
+                return false;
+            } else if (!livingEntity.isAlive()) {
+                return false;
+            } else if (!this.pauseWhenMobIdle) {
+                return !this.mob.getNavigation().isIdle();
+            } else {
+                return this.mob.isInPositionTargetRange(livingEntity.getBlockPos()) && !(livingEntity instanceof PlayerEntity playerEntity && (playerEntity.isSpectator() || playerEntity.isCreative()));
+            }
+        }
+
+        @Override
+        public void start() {
+            this.mob.getNavigation().startMovingAlong(this.path, this.speed);
+            this.mob.setAttacking(true);
+            this.updateCountdownTicks = 0;
+            this.cooldown = 0;
+        }
+
+        @Override
+        public void stop() {
+            LivingEntity livingEntity = this.mob.getTarget();
+            if (!EntityPredicates.EXCEPT_CREATIVE_OR_SPECTATOR.test(livingEntity)) {
+                this.mob.setTarget(null);
+            }
+
+            this.mob.setAttacking(false);
+            this.mob.getNavigation().stop();
+        }
+
+        @Override
+        public boolean shouldRunEveryTick() {
+            return true;
+        }
+
+        @Override
+        public void tick() {
+            LivingEntity livingEntity = this.mob.getTarget();
+            if (livingEntity != null) {
+                this.mob.getLookControl().lookAt(livingEntity, 30.0F, 30.0F);
+                this.updateCountdownTicks = Math.max(this.updateCountdownTicks - 1, 0);
+                if ((this.pauseWhenMobIdle || this.mob.getVisibilityCache().canSee(livingEntity))
+                        && this.updateCountdownTicks <= 0
+                        && (
+                        this.targetX == 0.0 && this.targetY == 0.0 && this.targetZ == 0.0
+                                || livingEntity.squaredDistanceTo(this.targetX, this.targetY, this.targetZ) >= 1.0
+                                || this.mob.getRandom().nextFloat() < 0.05F
+                )) {
+                    this.targetX = livingEntity.getX();
+                    this.targetY = livingEntity.getY();
+                    this.targetZ = livingEntity.getZ();
+                    this.updateCountdownTicks = 4 + this.mob.getRandom().nextInt(7);
+                    double d = this.mob.squaredDistanceTo(livingEntity);
+                    if (d > 1024.0) {
+                        this.updateCountdownTicks += 10;
+                    } else if (d > 256.0) {
+                        this.updateCountdownTicks += 5;
+                    }
+
+                    if (!this.mob.getNavigation().startMovingTo(livingEntity, this.speed)) {
+                        this.updateCountdownTicks += 15;
+                    }
+
+                    this.updateCountdownTicks = this.getTickCount(this.updateCountdownTicks);
+                }
+
+                this.cooldown = Math.max(this.cooldown - 1, 0);
+                this.attack(livingEntity);
+            }
+        }
+
+        protected void attack(LivingEntity target) {
+            if (this.canAttack(target)) {
+                this.resetCooldown();
+                this.mob.swingHand(Hand.MAIN_HAND);
+                this.mob.tryAttack(getServerWorld(this.mob), target);
+            }
+        }
+
+        protected void resetCooldown() {
+            this.cooldown = this.getTickCount(20);
+        }
+
+        protected boolean isCooledDown() {
+            return this.cooldown <= 0;
+        }
+
+        protected boolean canAttack(LivingEntity target) {
+            return this.isCooledDown() && this.mob.isInAttackRange(target) && this.mob.getVisibilityCache().canSee(target);
+        }
+
+        protected int getCooldown() {
+            return this.cooldown;
+        }
+
+        protected int getMaxCooldown() {
+            return this.getTickCount(20);
+        }
     }
 
     static class DaggerAttackGoal extends Goal {
